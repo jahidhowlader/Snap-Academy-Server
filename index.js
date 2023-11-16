d0c8b18207335d0ec18208c8b0c260284ff95d65
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-11-16 20:14:16 +0600

    delete unnessery code

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ app.use(cors())
 
 app.use(express.json())
 
-// Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Handle requests to all routes by serving the React app
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-
 app.get('/', async (req, res) => {
 
     res.send('SERVER IS RUNNING')
