9b55de1ed7346c1863b86c60dcc858ee0ee103d4
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-11-19 17:53:40 +0600

    remove unnessersery code

--- index.js
+++ index.js
@@ -52,8 +52,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
         // faziul
 
         const courseCollection = client.db("SnapAcademyDB").collection("courses");
@@ -203,13 +201,9 @@ async function run() {
             await subscribationCollection.insertOne({ email })
         })
 
-
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 run().catch(console.dir);
